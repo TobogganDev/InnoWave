b52502d6da9d461d31a79692db5ef770b8e2cb5e
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-09-29 09:18:19 +0200

    :art: ADD colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'custom-black': '#313235',
+      'custom-purple': '#7E49F2',
+      'custom-light-purple': '#A36BF2',
+      'custom-pink': '#EAACF2',
+      'custom-light-pink': '#F1C4F2',
+    },
     fontFamily: {
       'monoton': ['Monoton', 'cursive'],
       'notable': ['Notable', 'sans-serif'],
