08aad8f9b7b797728e3f5be49bfd3a8668492da1
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-10-19 11:28:58 +0200

    :bug: fix build problem

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 <template>
   <main class="w-full h-screen flex flex-col justify-center items-center">
     <div class="w-3/12">
-      <img src="images/logo.png" alt="logo InnoWave" class="w-full" />
+      <img src="/images/logo.png" alt="logo InnoWave" class="w-full" />
     </div>
     <h2 class="font-semibold text-5xl">Comming soon ... 🙈</h2>
   </main>
