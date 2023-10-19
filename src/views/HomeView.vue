79ab123516d04f030c23262be2b9002a1f0a7fb0
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-10-19 12:00:23 +0200

    :lipstick: update comming soon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
-  <main class="w-full h-screen flex flex-col justify-center items-center">
-    <div class="w-3/12">
+  <main class="w-full h-screen flex justify-center items-center">
+    <div class="w-1/12 absolute top-0 left-0">
       <img src="/images/logo.png" alt="logo InnoWave" class="w-full" />
     </div>
     <h2 class="font-semibold text-5xl">Comming soon ... 🙈</h2>
