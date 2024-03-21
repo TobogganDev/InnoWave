cf77495d1bc3cc7414164a2455a300db81552159
C00MAR <moureau.marc78@gmail.com>
2024-03-21 11:34:14 +0100

    :sparkles: Header Update + Json Improve :sparkles:

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -3,14 +3,12 @@
         <div class="max-w-md mx-auto flex flex-row items-center">
             <div class=" w-1/3 flex flex-row items-center justify-end gap-4 text-custom-black font-normal">
                 <router-link to="/" class="w-fit">Home</router-link>
-                <router-link to="/blog" class="w-fit">Blog</router-link>
             </div>
             <div class=" w-1/3 flex justify-center items-center">
                 <img src="/images/logo.png" alt="logo_innowave" class="w-1/2">
             </div>
             <div class="w-1/3 flex flex-row items-center justify-start gap-4 text-custom-black font-normal">
-                <router-link to="/a_propos" class="w-fit">A Propos</router-link>
-                <router-link to="/contact" class="w-fit">Contact</router-link>
+                <router-link to="/blog" class="w-fit">Blog</router-link>
             </div>
         </div>
 
